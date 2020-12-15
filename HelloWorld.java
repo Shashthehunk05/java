63fe44df9943ee57ac43b768b0a23b5eb386e1a7
shashi <shashikumargn@outlook.com>
2020-12-15 19:55:58 +0530

    Release7

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, I am here in the terminal window.
-      System.out.println(" Shashi is a great guy in devops ");
+      System.out.println(" Shashi is a great guy in devops too ");
    }
 }
